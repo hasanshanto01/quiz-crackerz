0f23a5c42d47a176336a0159426b54160dd96286
hasanshanto01 <hasanshanto01@gmail.com>
2022-10-11 22:05:22 +0600

    header section done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import img from '../../images/quiz-bg.jpg';
+
+const Header = () => {
+    return (
+        <div className='relative'>
+            <img src={img} alt="" className='w-full h-80 mx-auto' />
+            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white'>
+                <h2 className='text-2xl'>Welcome To Quizzora !!!</h2>
+                <p className='text-lg'>"Here, you can  test your knowledge on given topics"</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,47 +1,50 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Header from '../Header/Header';
 
 const NavBar = () => {
     return (
-        <div className="navbar bg-accent drop-shadow-xl">
-            <div className="flex-1 ml-20">
-                <NavLink to='/' className="btn btn-ghost normal-case text-3xl text-white">Quizzora</NavLink>
-            </div>
-            <div className="flex-none mr-20 text-white">
-                <ul className="flex gap-10 text-lg">
-                    <li className=''>
-                        <NavLink
-                            to="/topics"
-                            className={({ isActive }) =>
-                                isActive ? 'text-cyan-500' : undefined
-                            }
-                        >
-                            Topics
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/statistics"
-                            className={({ isActive }) =>
-                                isActive ? 'text-cyan-500' : undefined
-                            }
-                        >
-                            Statistics
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/blog"
-                            className={({ isActive }) =>
-                                isActive ? 'text-cyan-500' : undefined
-                            }
-                        >
-                            Blog
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-        </div >
+        <div>
+            <div className="navbar bg-accent drop-shadow-xl">
+                <div className="flex-1 ml-14">
+                    <NavLink to='/' className="btn btn-ghost normal-case text-3xl text-white">Quizzora</NavLink>
+                </div>
+                <div className="flex-none mr-14 text-white">
+                    <ul className="flex gap-10 text-lg font-semibold">
+                        <li className=''>
+                            <NavLink
+                                to="/topics"
+                                className={({ isActive }) =>
+                                    isActive ? 'text-cyan-500' : undefined
+                                }
+                            >
+                                Topics
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/statistics"
+                                className={({ isActive }) =>
+                                    isActive ? 'text-cyan-500' : undefined
+                                }
+                            >
+                                Statistics
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/blog"
+                                className={({ isActive }) =>
+                                    isActive ? 'text-cyan-500' : undefined
+                                }
+                            >
+                                Blog
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div >
+        </div>
     );
 };
 
